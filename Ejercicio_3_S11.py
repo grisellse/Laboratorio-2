ccffeda818005155a7b081f3e16e521c4d605a34
Yohana <142858041+grisellse@users.noreply.github.com>
2024-10-06 23:33:07 -0600

    Ejercicio_3_S11.py

--- Ejercicio_3_S11.py
+++ Ejercicio_3_S11.py
@@ -1,25 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# https://www.kaggle.com/datasets/shiivvvaam/top-youtuber-worldwide ---- Se uso este DataSet
+
+
 # Cargar el dataset
 df = pd.read_csv("Youtuber.csv")
 
-# Revisar las primeras filas del dataset
+# Revisar solo las primeras filas del dataset
 print(df.head())
 
-# Seleccionar los 10 YouTubers con más suscriptores
-top_youtubers = df[['Channel Name', 'Subscribers']].sort_values(by='Subscribers', ascending=False).head(10)
+# -------------------------------------------------------
+# -------------------grafico de barras-------------------
+# -------------------------------------------------------
 
-# Crear gráfico de barras
+top_youtubers = df[['Channel Name', 'Subscribers']].sort_values(by='Subscribers', ascending=False).head(10)
+# 
 plt.figure(figsize=(10, 6))
 plt.bar(top_youtubers['Channel Name'], top_youtubers['Subscribers'], color='skyblue')
 plt.title('Top 10 YouTubers con más Suscriptores')
 plt.xlabel('YouTuber')
 plt.ylabel('Número de Suscriptores (millones)')
 plt.xticks(rotation=45)
-plt.show()
+plt.show() # Muestra el gráfico
+
+# -------------------------------------------------------
+# ----------------- grafica de lineas---------------------
+# --------------------------------------------------------
+
 # Seleccionar los 10 YouTubers con más vistas totales
 top_youtubers_vistas = df[['Channel Name', 'Average Views']].sort_values(by='Average Views', ascending=False).head(10)
+
 
 # Preparar los datos para el gráfico de líneas
 x = range(1, len(top_youtubers_vistas) + 1)
@@ -34,6 +45,10 @@ plt.ylabel('Total de Vistas (miles de millones)')
 plt.xticks(ticks=x, labels=top_youtubers_vistas['Channel Name'], rotation=45)
 plt.show()
 
+# -------------------------------------------------------
+# ---------------Gráfico de pastel-------------------------
+# ---------------------------------------------------------
+
 
 # Seleccionar los 10 YouTubers con más suscriptores y sus categorías
 top_youtubers_categoria = df[['Channel Name', 'Category']].value_counts().head(10)
@@ -43,3 +58,4 @@ plt.figure(figsize=(8, 8))
 plt.pie(top_youtubers_categoria.values, labels=top_youtubers_categoria.index, autopct='%1.1f%%', startangle=90)
 plt.title('Proporción de Categorías en el Top 10 YouTubers')
 plt.show()
+
